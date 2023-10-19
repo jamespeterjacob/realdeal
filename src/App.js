18fc18587a89462d9ef1139f94e2faa547610ad1
cefer <cefer@DESKTOP-HU1O35U>
2023-10-19 14:35:25 -0700

    modified:   src/App.css modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 function App() {
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
+    
   return (
     
     <div className="App">
-
+<body>
 <div class="topnav" id="myTopnav">
   <a href="#home" class="active">Home</a>
   <a href="#news">News</a>
@@ -19,19 +32,14 @@ function App() {
       <h2>Typical Media Query Breakpoints</h2>
 <p class="example">Resize the browser window to see how the background color of this paragraph changes on different screen sizes.</p>
       </header>
+</body>
+
 
 
     </div>
   );
 }
 
+
 export default App;
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
