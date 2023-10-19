809671cc7d0c4e222cb33c7302b774e66e61f34b
cefer <cefer@DESKTOP-HU1O35U>
2023-10-19 14:27:10 -0700

    modified:   src/App.css modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
       <header className="App-header">
       <h2>Typical Media Query Breakpoints</h2>
 <p class="example">Resize the browser window to see how the background color of this paragraph changes on different screen sizes.</p>
       </header>
+
+
     </div>
   );
 }
 
 export default App;
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
