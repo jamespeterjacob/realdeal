4042c6d62f62f429b0d99d4cb3f56f2f056137ab
cefer <cefer@DESKTOP-HU1O35U>
2023-10-19 15:11:37 -0700

    modified:   src/App.js modified:   src/Navbar.css modified:   src/Navbar.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,17 @@ import './App.css';
 
 function App() {
 
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-    }
+  
     
   return (
     
     <div className="App">
-<body>
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
-      <header className="App-header">
+
+      {/* <header className="App-header">
       <h2>Typical Media Query Breakpoints</h2>
 <p class="example">Resize the browser window to see how the background color of this paragraph changes on different screen sizes.</p>
-      </header>
-</body>
+      </header> */}
+
 
 
 
