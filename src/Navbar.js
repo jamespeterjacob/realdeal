ef32cdddf67c15361625fb079545f076c91cd3a1
cefer <cefer@DESKTOP-HU1O35U>
2023-10-20 11:20:41 -0700

    modified:   src/Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
      <div>
         <h2>Brand</h2>
         <ul>
-            <li><a href="/">First</a></li>
-            <li><a href="/about">Second</a></li>
+            <li><a href="/realdeal/">First</a></li>
+            <li><a href="/realdeal/about">Second</a></li>
             <li><a>Third</a></li>
             </ul>
      </div>
