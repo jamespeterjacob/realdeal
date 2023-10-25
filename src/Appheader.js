55d6ecb3ab29d4df61973a893c1792cde860c15a
cefer <cefer@DESKTOP-HU1O35U>
2023-10-25 07:03:35 -0700

    modified:   src/Appheader.js

--- src/Appheader.js
+++ src/Appheader.js
@@ -41,11 +41,7 @@ const Appheader = () => {
                 <div className="container">
                <Sidebar/>
                </div>
-               <div className="main">
-                <Home/>
-                <Blogs/>
-                <Contact/>
-               </div>
+               
                </div>
               
                    
