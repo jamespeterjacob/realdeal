d51650b70ca9e594fa7b8b84eaba0dd53d47e78a
cefer <cefer@DESKTOP-HU1O35U>
2023-10-25 06:03:38 -0700

    modified:   src/App.js modified:   src/Appheader.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     <BrowserRouter>
     <Appheader></Appheader>
     <Routes>
-      <Route path='/' element={<Home/>}></Route>
+      <Route path='/home' element={<Home/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
       {/* <Route path='/register' element={<Register/>}></Route> */}
       {/* <Route path='/component/company' element={</Company>}></Route>
