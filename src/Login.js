291aa159b64110a7b2168b77fe1792faa2e40998
cefer <cefer@DESKTOP-HU1O35U>
2023-10-28 06:33:41 -0700

    modified:   src/Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,35 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const defaultTheme = createTheme();
+
+function Copyright(props) {
+    return (
+      <Typography variant="body2" color="text.secondary" align="center" {...props}>
+        {'Copyright © '}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    );
+  }
 
 const Login = () => {
     const [username, usernameupdate] = useState('');
@@ -96,45 +122,40 @@ sessionStorage.clear();
     return (
 
        
-        <Box className='row main' style={{alignContent:'center', alignItems:'center'}}
-       sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
-       noValidate autoComplete="off">
-                <form onSubmit={ProceedLogin} className="">
-                    {/* <div className="card">
-                        <div className="card-header">
-                            <h2>Login</h2>
-                        </div>
-                        <div className="card-body">
-                            <div className="form-group">
-                                <label>User Name <span className="errmsg">*</span></label>
-                                <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                            <div className="form-group">
-                                <label>Password <span className="errmsg">*</span></label>
-                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">Login</button> |
-                            <Link className="btn btn-success" to={'/register'}>New User</Link>
-                        </div>
-                    </div> */}
-                    
-                    <div className="container">
-                    <h2>Login</h2>
-                        <TextField type="text" name="username" value={username} onChange={e => usernameupdate(e.target.value)}></TextField>
-                        </div>
-                        <div>
-                        <TextField type="text" name="password" value={password} onChange={e => passwordupdate(e.target.value)}></TextField>
-                        </div>
-                        <div style={{ margin:'10px' }}>
-                        <Button variant="contained" type="submit" >Login</Button> 
-                            <Button variant="contained" style={{ margin:'20px' }}>
-                            <Link className="btn btn-success" to={'/register'} style={{ textDecoration: 'none', color: 'white' }}>Register</Link>
-                            </Button>
-                        </div>
-                </form>
-                </Box>
+        <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', }} >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" onSubmit={ProceedLogin} noValidate sx={{ mt: 1 }}>
+              <TextField margin="normal" required fullWidth id="username" label="Username" name="username" autoComplete="username" autoFocus type="text" value={username} onChange={e => usernameupdate(e.target.value)} />
+              <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" value={password} onChange={e => passwordupdate(e.target.value)} />
+              <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+              <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }} >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </ThemeProvider>
         
 
         
