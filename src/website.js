e5fec29909c8e9ca02e7b90d8f00b9fc2c92e41c
cefer <cefer@DESKTOP-HU1O35U>
2023-10-29 11:24:03 -0700

    new file:   src/AddressForm.js new file:   src/components/dashboard/chart.js new file:   src/components/dashboard/dashboard.js new file:   src/components/dashboard/deposits.js new file:   src/components/dashboard/listItems.js new file:   src/components/dashboard/orders.js new file:   src/components/dashboard/title.js new file:   src/website.js

---
+++ src/website.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Appheader from './Appheader';
+import Contact from './Contact';
+import JobList from './components/jobs/JobList';
+
+const Website = () => {
+
+    return (
+      <div>
+      <h2>Ceferin Web Solutions</h2>
+      <div className="topnav">
+        <a className="active" href="#home">Home</a>
+          <a href="/">Test</a>
+            <a href="/realdeal/#about">Test</a>
+              <a href="/login" class="split">Login</a>
+              </div>
+                 
+   </div> 
+     
+      );
+    };
+
+    export default Website;
